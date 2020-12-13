ad1f51f63d307942cf0ebaeb14a6d630a8c910a9
Zolmeister <zolikahan@gmail.com>
2020-12-13 17:43:58 -0600

    music: disable temporarily on window interaction

--- AutoHD/script_music.js
+++ AutoHD/script_music.js
@@ -1,7 +1,8 @@
+let disabledTimeout = null
 const loop = _ => {
   closedButton = document.querySelector('.ytmusic-you-there-renderer .yt-button-renderer')
-  if (closedButton) {
-    console.log('AutoHD: Awaken!')
+  if (closedButton && !disabledTimeout) {
+    console.log('AutoHD music: Awaken!')
     closedButton.click()
   }
   setTimeout(loop, 100)
@@ -9,3 +10,14 @@ const loop = _ => {
 
 console.log('AutoHD: starting awake loop')
 loop()
+
+window.addEventListener('click', _ => {
+  console.log('AutoHD music: disabled');
+  if (disabledTimeout) {
+    clearTimeout(disabledTimeout)
+  }
+  disabledTimeout = setTimeout(_ => {
+    console.log('AutoHD music: enabled');
+    disabledTimeout = null
+  }, 5000)
+})
